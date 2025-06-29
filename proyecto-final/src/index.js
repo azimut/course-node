37c28530796da816efdbfddb4c96874a39c2e0cd
azimut <azimut.github@protonmail.com>
2025-06-29 18:57:28 -0300

    init proyecto final

---
+++ proyecto-final/src/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import products from "./routes/products.js";
+
+const PORT = 3030;
+const app = express();
+
+app.use(express.json());
+app.use("/products", products);
+
+app.use((_req, res) => res.status(404).json({ error: "undefined path" }));
+app.listen(PORT, () => console.log(`http://0.0.0.0:${PORT}`));
