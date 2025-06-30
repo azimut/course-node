6c73fcea471be754225d2b22a0c4f209b373bd3d
azimut <azimut.github@protonmail.com>
2025-06-29 21:14:30 -0300

    init more routes

--- proyecto-final/src/routes/products.js
+++ proyecto-final/src/routes/products.js
@@ -2,6 +2,29 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/", (_req, res) => res.json({ message: "sucessfully created" }));
+router.get("/", (_req, res) => {
+  const products = [];
+  res.json(products);
+});
+router.get("/:id", (req, res) => {
+  const product = {};
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ error: "product not found" });
+  }
+});
+router.post("/", (req, res) => {
+  try {
+    const { name, price } = req.params;
+    const id = 999;
+    res.status(201).json({ id });
+  } catch (err) {
+    res.status(400).json({ error: "not enough params" });
+  }
+});
+router.delete("/:id", (req, res) => {
+  res.status(202).json({ message: "sucessfully deleted" });
+});
 
 export default router;
