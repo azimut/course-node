072c70c0560a986a74893f8a6b459b16e02422ad
azimut <azimut.github@protonmail.com>
2025-07-07 11:55:13 -0300

    routes: validation middleware, add failure tests

--- proyecto-final/src/routes/products.js
+++ proyecto-final/src/routes/products.js
@@ -1,39 +1,60 @@
 import { Router } from "express";
-import { body, param, query, oneOf, checkExact } from "express-validator";
+import {
+  body,
+  param,
+  query,
+  oneOf,
+  checkExact,
+  validationResult,
+} from "express-validator";
 
 import * as controller from "../controllers/products.js";
 
-const validateId = param("id").toInt();
+const validateId = param("id").isInt().toInt();
 const validateProduct = [
   body("name").isAlphanumeric(),
   body("brand").isAlphanumeric(),
   body("price").isNumeric().toFloat(),
 ];
-const validateSearch = [
+const validateSearch = oneOf([
   query("name").isAlphanumeric(),
   query("brand").isAlphanumeric(),
   query("minPrice").isNumeric().toFloat(),
   query("maxPrice").isNumeric().toFloat(),
-];
+]);
+const exitOnError = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty())
+    return res.status(400).json({ errors: errors.array() });
+  next();
+};
 
 const router = Router();
 router.get("/", controller.getProducts);
-router.get("/search", oneOf(validateSearch), controller.searchProduct);
-router.get("/:id", validateId, controller.getProduct);
-router.post("/", validateProduct, checkExact(), controller.postProduct);
+router.get("/search", validateSearch, exitOnError, controller.searchProduct);
+router.get("/:id", validateId, exitOnError, controller.getProduct);
+router.delete("/:id", validateId, exitOnError, controller.deleteProduct);
+router.post(
+  "/",
+  validateProduct,
+  checkExact(),
+  exitOnError,
+  controller.postProduct
+);
 router.put(
   "/:id",
   validateId,
   validateProduct,
   checkExact(),
+  exitOnError,
   controller.putProduct
 );
 router.patch(
   "/:id",
   validateId,
   oneOf(validateProduct),
+  exitOnError,
   controller.patchProduct
 );
-router.delete("/:id", validateId, controller.deleteProduct);
 
 export default router;
