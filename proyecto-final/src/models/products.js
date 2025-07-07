938d238a564daed30724b8652d07eb345c48e83b
azimut <azimut.github@protonmail.com>
2025-07-07 09:32:42 -0300

    refactor: move search to model, rm service

--- proyecto-final/src/models/products.js
+++ proyecto-final/src/models/products.js
@@ -37,6 +37,15 @@ export async function addNewProduct(product) {
   });
 }
 
+export async function searchProduct({ name, brand, minPrice, maxPrice }) {
+  let result = await getProducts();
+  if (name) result = result.filter((p) => p.name.includes(name));
+  if (brand) result = result.filter((p) => p.brand.includes(brand));
+  if (minPrice) result = result.filter((p) => p.price > minPrice);
+  if (maxPrice) result = result.filter((p) => p.price < maxPrice);
+  return result;
+}
+
 async function setProducts(products) {
   await fs.writeFile(productsFilename, JSON.stringify(products));
 }
