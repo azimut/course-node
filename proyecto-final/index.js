4b6420713620df519dff5fb3cabc4026133935fb
azimut <azimut.github@protonmail.com>
2025-07-07 10:48:48 -0300

    readme

--- proyecto-final/index.js
+++ proyecto-final/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import status from "http-status";
+import http from "http-status";
 import products from "./src/routes/products.js";
 
 const PORT = 3030;
@@ -9,8 +9,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/products", products);
-
 app.use((_req, res) =>
-  res.status(status.NOT_FOUND).json({ error: status[status.NOT_FOUND] })
+  res.status(http.NOT_FOUND).json({ error: http[http.NOT_FOUND] })
 );
+
 app.listen(PORT, () => console.log(`http://0.0.0.0:${PORT}`));
