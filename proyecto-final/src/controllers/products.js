38d93d6332098439d3deb38d49c7dd652c1f95ab
azimut <azimut.github@protonmail.com>
2025-07-07 06:52:41 -0300

    refactor: readability (?

--- proyecto-final/src/controllers/products.js
+++ proyecto-final/src/controllers/products.js
@@ -1,4 +1,4 @@
-import status from "http-status";
+import http from "http-status";
 import { validationResult } from "express-validator";
 import * as service from "../services/products.js";
 import * as model from "../models/products.js";
@@ -10,21 +10,26 @@ export async function getProducts(_req, res) {
 
 export async function getProduct(req, res) {
   const product = await model.getProduct(req.params.id);
-  if (product) res.json(product);
-  else res.status(status.NOT_FOUND).json({});
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(http.NOT_FOUND).json({});
+  }
 }
 
 export async function searchProduct(req, res) {
   const result = validationResult(req);
   if (result.isEmpty()) {
-    const results = await service.searchProduct(req.query);
-    res.json(results);
-  } else res.status(status.BAD_REQUEST).json({ errors: result.array() });
+    const search = await service.searchProduct(req.query);
+    res.json(search);
+  } else {
+    res.status(http.BAD_REQUEST).json({ errors: result.array() });
+  }
 }
 
 export async function deleteProduct(req, res) {
   await model.deleteProduct(req.params.id);
-  res.status(status.NO_CONTENT).send();
+  res.status(http.NO_CONTENT).send();
 }
 
 export async function createProduct(req, res) {
@@ -32,8 +37,8 @@ export async function createProduct(req, res) {
   if (result.isEmpty()) {
     await model.addNewProduct(req.body);
     const products = await model.getAllProducts();
-    res.status(status.CREATED).json(products);
-  } else res.status(status.BAD_REQUEST).json({ errors: result.array() });
+    res.status(http.CREATED).json(products);
+  } else res.status(http.BAD_REQUEST).json({ errors: result.array() });
 }
 
 export async function updateProduct(req, res) {
@@ -41,8 +46,8 @@ export async function updateProduct(req, res) {
   if (result.isEmpty()) {
     const existed = await service.existsProduct(req.params.id);
     await model.addProduct({ id: req.params.id, ...req.body });
-    res.status(existed ? status.NO_CONTENT : status.CREATED).send();
-  } else res.status(status.BAD_REQUEST).json({ errors: result.array() });
+    res.status(existed ? http.NO_CONTENT : http.CREATED).send();
+  } else res.status(http.BAD_REQUEST).json({ errors: result.array() });
 }
 
 export async function patchProduct(req, res) {
@@ -51,7 +56,7 @@ export async function patchProduct(req, res) {
     const exists = await service.existsProduct(req.params.id);
     if (exists) {
       await service.patchProduct({ id: req.params.id, ...req.body });
-      res.status(status.NO_CONTENT).send();
-    } else res.status(status.NOT_FOUND).send();
-  } else res.status(status.BAD_REQUEST).json({ errors: result.array() });
+      res.status(http.NO_CONTENT).send();
+    } else res.status(http.NOT_FOUND).send();
+  } else res.status(http.BAD_REQUEST).json({ errors: result.array() });
 }
