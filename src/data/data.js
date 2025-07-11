b086b8a405b4c78711a48c167aa22ad610fadbee
azimut <azimut.github@protonmail.com>
2025-07-10 21:20:11 -0300

    add .env-sample, extra FIREBASE_ envvars

--- src/data/data.js
+++ src/data/data.js
@@ -1,20 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import {
-  appId,
-  apiKey,
-  authDomain,
-  storageBucket,
-} from "../utils/constants.js";
+import { firebaseConfig } from "../utils/constants.js";
 
-const app = initializeApp({
-  apiKey,
-  appId,
-  authDomain,
-  storageBucket,
-  projectId: "curso-node-final",
-  messagingSenderId: "1075591860909",
-});
+const app = initializeApp({ ...firebaseConfig });
 const db = getFirestore(app);
 
 export { db };
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -11,3 +11,13 @@ export const apiKey = process.env.FIREBASE_API_KEY;
 export const authDomain = process.env.FIREBASE_AUTH_DOMAIN;
 export const storageBucket = process.env.FIREBASE_STORAGE_BUCKET;
 export const appId = process.env.FIREBASE_APP_ID;
+export const projectId = process.env.FIREBASE_PROJECT_ID;
+export const messagingSenderId = process.env.FIREBASE_MESSAGING_SENDER_ID;
+export const firebaseConfig = {
+  apiKey,
+  authDomain,
+  storageBucket,
+  appId,
+  projectId,
+  messagingSenderId,
+};
