d1a54b9cd3837e27c1e9ecf805bcdfd1b909e403
azimut <azimut.github@protonmail.com>
2025-07-17 16:40:11 -0300

    docs: add swagger https +method+ schemes
    this is needed to avoid "Mixed Content" in production

--- src/utils/swagger.js
+++ src/utils/swagger.js
@@ -7,7 +7,7 @@ const doc = {
     description: "Una simple API REST de productos usando express.js.",
   },
   host: `localhost:${port}`,
-  methods: ["https", "http"],
+  schemes: ["https", "http"],
   definitions: {
     SampleLogin: {
       $email: default_user.email,
