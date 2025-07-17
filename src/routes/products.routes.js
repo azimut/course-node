58580f89a82692da8b65e3287bd501ca7d2f9207
azimut <azimut.github@protonmail.com>
2025-07-17 10:14:52 -0300

    refactor: validations and routes

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,59 +1,15 @@
 import { Router } from "express";
-import { exitOnError } from "../middlewares/validator.js";
-import { body, param, query, oneOf, checkExact } from "express-validator";
-
+import * as validate from "../middlewares/validator.js";
 import * as controller from "../controllers/products.controller.js";
-
-const validateId = param("id").isAlphanumeric();
-const validateProduct = [
-  body("name").isAlphanumeric(),
-  body("categories").isArray(),
-  body("price").isNumeric().toFloat(),
-];
-const validateSearch = oneOf([
-  query("name").isAlphanumeric(),
-  query("category").isAlphanumeric(),
-  query("minPrice").isNumeric().toFloat(),
-  query("maxPrice").isNumeric().toFloat(),
-]);
 
 const router = Router();
 router.get("/products", controller.getProducts);
-router.get(
-  "/products/search",
-  validateSearch,
-  exitOnError,
-  controller.searchProduct
-);
-router.get("/products/:id", validateId, exitOnError, controller.getProduct);
+router.get("/products/search", validate.search, controller.searchProduct);
+router.get("/products/:id", validate.get, controller.getProduct);
 router.delete("/products/all", controller.deleteProducts);
-router.delete(
-  "/products/:id",
-  validateId,
-  exitOnError,
-  controller.deleteProduct
-);
-router.post(
-  "/products/create",
-  validateProduct,
-  checkExact(),
-  exitOnError,
-  controller.postProduct
-);
-router.put(
-  "/products/:id",
-  validateId,
-  validateProduct,
-  checkExact(),
-  exitOnError,
-  controller.putProduct
-);
-router.patch(
-  "/products/:id",
-  validateId,
-  oneOf(validateProduct),
-  exitOnError,
-  controller.patchProduct
-);
+router.delete("/products/:id", validate.delete_, controller.deleteProduct);
+router.post("/products/create", validate.post, controller.postProduct);
+router.put("/products/:id", validate.put, controller.putProduct);
+router.patch("/products/:id", validate.patch, controller.patchProduct);
 
 export default router;
