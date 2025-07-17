3aead82706d6be6f2960236033d26d78bf7b6293
azimut <azimut.github@protonmail.com>
2025-07-17 16:38:38 -0300

    docs: add swagger https method

--- src/utils/swagger.js
+++ src/utils/swagger.js
@@ -7,6 +7,7 @@ const doc = {
     description: "Una simple API REST de productos usando express.js.",
   },
   host: `localhost:${port}`,
+  methods: ["https", "http"],
   definitions: {
     SampleLogin: {
       $email: default_user.email,
