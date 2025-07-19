0e9fb7bec882bf939686d6a0c3f1fea4c1c69298
azimut <azimut.github@protonmail.com>
2025-07-18 23:27:14 -0300

    test: add init/cleanup tasks

--- test/hooks.cjs
+++ test/hooks.cjs
@@ -1,8 +1,14 @@
 const { terminate } = require("firebase/firestore");
 const { db } = require("../src/data/data");
 const { server } = require("../index");
+const { deleteProducts, addNewProduct } = require("../src/models/products");
+const data = require("./init.json");
 
 exports.mochaHooks = {
+  beforeAll: async () => {
+    await deleteProducts();
+    data.forEach(addNewProduct);
+  },
   afterAll: async () => {
     await terminate(db);
     server.close();
